8fa2ba2f3f5d54201a981d54298abb807d0ec604
Paul-Elie Pipelin <paul.elie97@gmail.com>
2025-03-10 22:01:14 +0100

    Switch containers to `Unmanaged` to match `0.14.0`

--- src/tests_movegen.zig
+++ src/tests_movegen.zig
@@ -12,13 +12,10 @@ const allocator = std.testing.allocator;
 
 test "Perft" {
     tables.initAll(allocator);
-    defer tables.deinitAll();
+    defer tables.deinitAll(allocator);
 
     var s: position.State = position.State{};
     var pos: position.Position = try position.Position.setFen(&s, position.start_fen);
-
-    var list = std.ArrayList(types.Move).init(allocator);
-    defer list.deinit();
 
     try expectEqual(20, search.perft(std.testing.allocator, &pos, 1, false) catch unreachable);
     try expectEqual(400, search.perft(std.testing.allocator, &pos, 2, false) catch unreachable);
@@ -31,13 +28,10 @@ test "Perft" {
 
 test "PerftKiwipete" {
     tables.initAll(allocator);
-    defer tables.deinitAll();
+    defer tables.deinitAll(allocator);
 
     var s: position.State = position.State{};
     var pos: position.Position = try position.Position.setFen(&s, position.kiwi_fen);
-
-    var list = std.ArrayList(types.Move).init(allocator);
-    defer list.deinit();
 
     try expectEqual(48, search.perft(std.testing.allocator, &pos, 1, false) catch unreachable);
     try expectEqual(2039, search.perft(std.testing.allocator, &pos, 2, false) catch unreachable);
@@ -48,13 +42,10 @@ test "PerftKiwipete" {
 
 test "PerftPos3" {
     tables.initAll(allocator);
-    defer tables.deinitAll();
+    defer tables.deinitAll(allocator);
 
     var s: position.State = position.State{};
     var pos: position.Position = try position.Position.setFen(&s, "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -");
-
-    var list = std.ArrayList(types.Move).init(allocator);
-    defer list.deinit();
 
     try expectEqual(14, search.perft(std.testing.allocator, &pos, 1, false) catch unreachable);
     try expectEqual(191, search.perft(std.testing.allocator, &pos, 2, false) catch unreachable);
@@ -65,13 +56,10 @@ test "PerftPos3" {
 
 test "PerftPos4" {
     tables.initAll(allocator);
-    defer tables.deinitAll();
+    defer tables.deinitAll(allocator);
 
     var s: position.State = position.State{};
     var pos: position.Position = try position.Position.setFen(&s, "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
-
-    var list = std.ArrayList(types.Move).init(allocator);
-    defer list.deinit();
 
     try expectEqual(6, search.perft(std.testing.allocator, &pos, 1, false) catch unreachable);
     try expectEqual(264, search.perft(std.testing.allocator, &pos, 2, false) catch unreachable);
@@ -82,13 +70,10 @@ test "PerftPos4" {
 
 test "PerftPos5" {
     tables.initAll(allocator);
-    defer tables.deinitAll();
+    defer tables.deinitAll(allocator);
 
     var s: position.State = position.State{};
     var pos: position.Position = try position.Position.setFen(&s, "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
-
-    var list = std.ArrayList(types.Move).init(allocator);
-    defer list.deinit();
 
     try expectEqual(44, search.perft(std.testing.allocator, &pos, 1, false) catch unreachable);
     try expectEqual(1_486, search.perft(std.testing.allocator, &pos, 2, false) catch unreachable);
@@ -99,82 +84,82 @@ test "PerftPos5" {
 
 test "MovegenEnPassant" {
     tables.initAll(allocator);
-    defer tables.deinitAll();
+    defer tables.deinitAll(allocator);
 
     var s: position.State = position.State{};
     var pos: position.Position = try position.Position.setFen(&s, "4k3/8/8/3pPp2/8/8/8/4K3 w - d6 0 3");
 
-    var list = std.ArrayList(types.Move).init(allocator);
-    defer list.deinit();
+    var list: std.ArrayListUnmanaged(types.Move) = .empty;
+    defer list.deinit(allocator);
 
-    pos.generateLegalMoves(pos.state.turn, &list);
+    pos.generateLegalMoves(allocator, pos.state.turn, &list);
 
     try expectEqual(7, list.items.len);
 }
 
 test "MovegenBishop" {
     tables.initAll(allocator);
-    defer tables.deinitAll();
+    defer tables.deinitAll(allocator);
 
     var s: position.State = position.State{};
     var pos: position.Position = try position.Position.setFen(&s, "3k4/8/8/B5BB/8/1B4B1/5B2/2BKB3 w - - 0 1");
 
-    var list = std.ArrayList(types.Move).init(allocator);
-    defer list.deinit();
+    var list: std.ArrayListUnmanaged(types.Move) = .empty;
+    defer list.deinit(allocator);
 
-    pos.generateLegalMoves(pos.state.turn, &list);
+    pos.generateLegalMoves(allocator, pos.state.turn, &list);
 
     try expectEqual(52, list.items.len);
 }
 
 test "MovegenRook" {
     tables.initAll(allocator);
-    defer tables.deinitAll();
+    defer tables.deinitAll(allocator);
 
     var s: position.State = position.State{};
     var pos: position.Position = try position.Position.setFen(&s, "3k4/8/8/R5RR/8/1R4R1/5R2/2RKR3 w - - 0 1");
 
-    var list = std.ArrayList(types.Move).init(allocator);
-    defer list.deinit();
+    var list: std.ArrayListUnmanaged(types.Move) = .empty;
+    defer list.deinit(allocator);
 
-    pos.generateLegalMoves(pos.state.turn, &list);
+    pos.generateLegalMoves(allocator, pos.state.turn, &list);
 
     try expectEqual(84, list.items.len);
 }
 
 test "MovegenSliders" {
     tables.initAll(allocator);
-    defer tables.deinitAll();
+    defer tables.deinitAll(allocator);
 
     var s: position.State = position.State{};
     var pos: position.Position = try position.Position.setFen(&s, "3k4/4R3/3B4/1Q6/8/5R2/2Q1B3/1Q1K1R2 w - - 0 1");
 
-    var list = std.ArrayList(types.Move).init(allocator);
-    defer list.deinit();
+    var list: std.ArrayListUnmanaged(types.Move) = .empty;
+    defer list.deinit(allocator);
 
-    pos.generateLegalMoves(pos.state.turn, &list);
+    pos.generateLegalMoves(allocator, pos.state.turn, &list);
 
     try expectEqual(86, list.items.len);
 }
 
 test "MovegenKing" {
     tables.initAll(allocator);
-    defer tables.deinitAll();
+    defer tables.deinitAll(allocator);
 
     var s: position.State = position.State{};
     var pos: position.Position = try position.Position.setFen(&s, "3qkr2/8/8/8/8/8/8/4K3 w - - 0 1");
 
-    var list = std.ArrayList(types.Move).init(allocator);
-    defer list.deinit();
+    var list: std.ArrayListUnmanaged(types.Move) = .empty;
+    defer list.deinit(allocator);
 
-    pos.generateLegalMoves(pos.state.turn, &list);
+    pos.generateLegalMoves(allocator, pos.state.turn, &list);
 
     try expectEqual(1, list.items.len);
 
-    list.clearAndFree();
+    list.clearAndFree(allocator);
 
     pos = try position.Position.setFen(&s, "3qk3/8/8/8/8/8/8/R3K2R w KQ - 0 1");
-    pos.generateLegalMoves(pos.state.turn, &list);
+    pos.generateLegalMoves(allocator, pos.state.turn, &list);
 
     try expectEqual(23, list.items.len);
 }
