8a3b0d9554b5bbb3632fd0754d1d34a8b14407c0
Paul-Elie Pipelin <paul.elie97@gmail.com>
2025-12-24 13:20:34 +0100

    f if better to squash to Change self.scores type to avoid allocations

--- src/movepick.zig
+++ src/movepick.zig
@@ -23,7 +23,7 @@ pub const MovePick = struct {
     tt_move: types.Move = types.Move.none,
     index_capture: u8 = 0,
     index_quiet: u8 = 0,
-    last_positive_capture: usize = 0,
+    first_negative_capture: usize = 0,
 
     pub fn nextMove(self: *MovePick, allocator: std.mem.Allocator, pos: *position.Position, pv_move: types.Move, comptime is_960: bool) !types.Move {
         if (self.stage == 0 or self.stage == 10) {
@@ -85,7 +85,7 @@ pub const MovePick = struct {
             position.orderMoves(self.moves_capture.items, &scores);
             for (self.moves_capture.items, 0..) |move, i| {
                 if (pos.board[move.getFrom().index()].pieceToPieceType().index() > pos.board[move.getTo().index()].pieceToPieceType().index() or move.isEnPassant()) {
-                    self.last_positive_capture = i;
+                    self.first_negative_capture = i;
                     break;
                 }
             }
@@ -99,7 +99,7 @@ pub const MovePick = struct {
 
         // Positive captures
         if (self.stage == 4) {
-            if (self.index_capture <= self.last_positive_capture) {
+            if (self.index_capture < self.first_negative_capture) {
                 const move: types.Move = self.moves_capture.items[self.index_capture];
                 self.index_capture += 1;
                 return move;
@@ -181,6 +181,6 @@ pub const MovePick = struct {
         self.tt_move = types.Move.none;
         self.index_capture = 0;
         self.index_quiet = 0;
-        self.last_positive_capture = 0;
+        self.first_negative_capture = 0;
     }
 };
