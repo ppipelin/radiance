d8ee180f879e85b2d1fe0562305fee0ca57a795d
Paul-Elie Pipelin <paul.elie97@gmail.com>
2026-02-17 00:20:24 +0100

    tmp

--- src/tune.zig
+++ src/tune.zig
@@ -3,6 +3,7 @@ const position = @import("position.zig");
 const std = @import("std");
 const types = @import("types.zig");
 const tables = @import("tables.zig");
+const variable = @import("variable.zig");
 
 const Tuple = std.meta.Tuple;
 const Triplet = Tuple(&.{ []const u8, Wdl });
@@ -61,9 +62,11 @@ fn readBook(allocator: std.mem.Allocator) !std.ArrayList(Triplet) {
         null,
     );
 
-    var buffer: [10_000]u8 = undefined;
+    var buffer: []u8 = try allocator.alloc(u8, 1_000_000 * 64);
+    // var buffer: []u8 = try allocator.alloc(u8, 50 * 64);
+    defer allocator.free(buffer);
 
-    const len = try std.os.windows.ReadFile(file_handle, &buffer, null);
+    const len = try std.os.windows.ReadFile(file_handle, buffer, null);
 
     var list: std.ArrayList(Triplet) = .empty;
 
@@ -71,7 +74,6 @@ fn readBook(allocator: std.mem.Allocator) !std.ArrayList(Triplet) {
     while (it.next()) |token| {
         if (std.mem.endsWith(u8, token[0..(token.len - 1)], "]")) {
             const wdl = try std.fmt.parseFloat(f16, token[(token.len - 5)..(token.len - 2)]);
-            std.debug.print("wdl {}\n", .{wdl});
             const copy = try allocator.dupe(u8, token[0..(token.len - 7)]);
             try list.append(allocator, .{ copy, Wdl.init(wdl) });
         }
@@ -80,28 +82,135 @@ fn readBook(allocator: std.mem.Allocator) !std.ArrayList(Triplet) {
 }
 
 fn sigmoid(value: f32) f32 {
-    // const lambda: types.Value = 1.0;
-    const lambda: f32 = 0.5;
-    return @divTrunc(1.0, 1.0 + @exp(lambda * -value));
+    const lambda: types.Value = 1.0;
+    // const lambda: f32 = 0.5;
+    return 1.0 / (1.0 + @exp(lambda * -value / 100));
 }
 
-fn eval(book: std.ArrayList(Triplet)) !void {
+fn eval(book: std.ArrayList(Triplet)) !f32 {
     var difference: f32 = 0;
     for (book.items) |triplet| {
         var s: position.State = position.State{};
         const pos: position.Position = try position.Position.setFen(&s, triplet[0]);
-        difference += std.math.pow(f32, sigmoid(@floatFromInt(evaluate.evaluateTable(pos))) - triplet[1].index(), 2);
+        const multiply: types.Value = if (pos.state.turn == .white) 1 else -1;
+        // std.debug.print("eval {}, sigm {}, real {}, error^2 {}\n", .{ multiply * evaluate.evaluateTable(pos), sigmoid(@floatFromInt(multiply * evaluate.evaluateTable(pos))), triplet[1].index(), std.math.pow(f32, sigmoid(@floatFromInt(multiply * evaluate.evaluateTable(pos))) - triplet[1].index(), 2) });
+        difference += std.math.pow(f32, sigmoid(@floatFromInt(multiply * evaluate.evaluateTable(pos))) - triplet[1].index(), 2);
     }
     std.debug.print("mean difference {}\n", .{difference / @as(f32, @floatFromInt(book.items.len))});
+    return difference;
+}
+
+fn updateVariable(variable_new: []const types.Value) void {
+    variable.knight_mobility = variable_new[0];
+    variable.bishop_mobility = variable_new[1];
+    variable.rook_mobility = variable_new[2];
+    variable.queen_mobility = variable_new[3];
+    variable.king_mobility = variable_new[4];
+    variable.pawn_threat_knight = variable_new[5];
+    variable.pawn_threat_bishop = variable_new[6];
+    variable.pawn_threat_rook = variable_new[7];
+    variable.pawn_threat_queen = variable_new[8];
+    variable.pawn_defend_king = variable_new[9];
+    variable.pawn_isolated = variable_new[10];
+    variable.pawn_doubled = variable_new[11];
+    variable.pawn_blocked = variable_new[12];
+    variable.pawn_protection = variable_new[13];
+    variable.bishop_pair = variable_new[14];
+    variable.rook_open_files = variable_new[15];
+    variable.rook_semi_open_files = variable_new[16];
+}
+
+// fn perturbate(variable_new: []types.Value, magnitude: f32) !void {
+//     // var prng = std.Random.DefaultPrng.init(@bitCast(std.time.microTimestamp()));
+//     // prng.random()
+
+//     var prng = std.Random.DefaultPrng.init(seed: {
+//         var seed: u64 = undefined;
+//         // get random seed from OS
+//         try std.posix.getrandom(std.mem.asBytes(&seed));
+//         break :seed seed;
+//     });
+//     const rand = prng.random();
+
+//     for (variable_new) |*current_var| {
+//         current_var.* += (rand.intRangeAtMost(types.Value, -1, 1) * magnitude);
+//     }
+// }
+
+fn computeDelta(deltas: []types.Value, magnitude: types.Value) !void {
+    var prng = std.Random.DefaultPrng.init(seed: {
+        var seed: u64 = undefined;
+        // get random seed from OS
+        try std.posix.getrandom(std.mem.asBytes(&seed));
+        break :seed seed;
+    });
+    const rand = prng.random();
+
+    for (deltas) |*current_delta| {
+        if (rand.boolean()) {
+            current_delta.* += magnitude;
+        } else {
+            current_delta.* -= magnitude;
+        }
+    }
+}
+
+fn applyDelta(variable_new: []types.Value, deltas: []const types.Value) void {
+    for (variable_new, 0..) |*variable_current, i| {
+        variable_current.* += deltas[i];
+    }
+}
+
+fn applyDeltaNegative(variable_new: []types.Value, deltas: []const types.Value) void {
+    for (variable_new, 0..) |*variable_current, i| {
+        variable_current.* -= deltas[i];
+    }
 }
 
 pub fn run(allocator: std.mem.Allocator, stdout: *std.Io.Writer) !void {
-    try stdout.print("begin tune\n", .{});
     var book: std.ArrayList(Triplet) = try readBook(allocator);
     defer book.deinit(allocator);
 
-    for (book.items) |fen| {
-        std.debug.print("{s}, {}\n", .{ fen[0], fen[1] });
+    // Set vars
+    var initial: [17]types.Value = .{ 5, 5, 5, 5, 5, 0, 0, 0, 0, 5, 30, 15, 10, 30, 10, 40, 20 };
+    // var initial: [17]types.Value = .{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    var tmp: [17]types.Value = initial;
+    var delta: [17]types.Value = @splat(0);
+
+    const alpha: f32 = 0.602;
+    const gamma: f32 = 0.101;
+
+    const a: f32 = 0.1;
+    const c: f32 = 1;
+    const A: f32 = 1;
+
+    for (0..2) |k_| {
+        const k: f32 = @floatFromInt(k_);
+        const ak = a / std.math.pow(f32, k + 1 + A, alpha);
+        const ck = c / std.math.pow(f32, k + 1, gamma);
+
+        const magnitude: types.Value = @intFromFloat(@max(@min(ck, std.math.maxInt(types.Value)), std.math.minInt(types.Value)));
+        try computeDelta(&delta, magnitude);
+
+        applyDelta(&tmp, delta[0..]);
+        updateVariable(tmp[0..17]);
+        const v1 = try eval(book);
+
+        applyDeltaNegative(&tmp, delta[0..]);
+        updateVariable(tmp[0..17]);
+        const v2 = try eval(book);
+
+        const match = v1 - v2;
+
+        for (&initial, 0..) |*param, i| {
+            const variation: types.Value = @intFromFloat(@max(@min(ak * match / (ck * @as(f32, @floatFromInt(delta[i]))), std.math.maxInt(types.Value)), std.math.minInt(types.Value)));
+            param.* += variation;
+        }
     }
-    try eval(book);
+
+    try stdout.print("Found variables:\n", .{});
+    for (initial) |param| {
+        try stdout.print("{}, ", .{param});
+    }
+    try stdout.print("\n", .{});
 }
--- src/variable.zig
+++ src/variable.zig
@@ -1,21 +1,24 @@
-pub const knight_mobility = 5;
-pub const bishop_mobility = 5;
-pub const rook_mobility = 5;
-pub const queen_mobility = 5;
-pub const king_mobility = 5;
+const types = @import("types.zig");
+const Value = types.Value;
 
-pub const pawn_threat_knight = 0;
-pub const pawn_threat_bishop = 0;
-pub const pawn_threat_rook = 0;
-pub const pawn_threat_queen = 0;
+pub var knight_mobility: Value = 5;
+pub var bishop_mobility: Value = 5;
+pub var rook_mobility: Value = 5;
+pub var queen_mobility: Value = 5;
+pub var king_mobility: Value = 5;
 
-pub const pawn_defend_king = 5;
-pub const pawn_isolated = 30;
-pub const pawn_doubled = 15;
-pub const pawn_blocked = 10;
-pub const pawn_protection = 30;
+pub var pawn_threat_knight: Value = 0;
+pub var pawn_threat_bishop: Value = 0;
+pub var pawn_threat_rook: Value = 0;
+pub var pawn_threat_queen: Value = 0;
 
-pub const bishop_pair = 10;
+pub var pawn_defend_king: Value = 5;
+pub var pawn_isolated: Value = 30;
+pub var pawn_doubled: Value = 15;
+pub var pawn_blocked: Value = 10;
+pub var pawn_protection: Value = 30;
 
-pub const rook_open_files = 40;
-pub const rook_semi_open_files = 20;
+pub var bishop_pair: Value = 10;
+
+pub var rook_open_files: Value = 40;
+pub var rook_semi_open_files: Value = 20;
