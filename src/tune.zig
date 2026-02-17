240b9932fc85cafe261e133750348834db6d7271
Paul-Elie Pipelin <paul.elie97@gmail.com>
2026-02-17 13:53:24 +0100

    tmp

--- src/tune.zig
+++ src/tune.zig
@@ -101,23 +101,9 @@ fn eval(book: []const Triplet) !f32 {
 }
 
 fn updateVariable(variable_new: []const types.Value) void {
-    variable.knight_mobility = variable_new[0];
-    variable.bishop_mobility = variable_new[1];
-    variable.rook_mobility = variable_new[2];
-    variable.queen_mobility = variable_new[3];
-    variable.king_mobility = variable_new[4];
-    variable.pawn_threat_knight = variable_new[5];
-    variable.pawn_threat_bishop = variable_new[6];
-    variable.pawn_threat_rook = variable_new[7];
-    variable.pawn_threat_queen = variable_new[8];
-    variable.pawn_defend_king = variable_new[9];
-    variable.pawn_isolated = variable_new[10];
-    variable.pawn_doubled = variable_new[11];
-    variable.pawn_blocked = variable_new[12];
-    variable.pawn_protection = variable_new[13];
-    variable.bishop_pair = variable_new[14];
-    variable.rook_open_files = variable_new[15];
-    variable.rook_semi_open_files = variable_new[16];
+    for (variable_new, 0..) |variable_current, i| {
+        variable.tunables[i].default = variable_current;
+    }
 }
 
 // fn perturbate(variable_new: []types.Value, magnitude: f32) !void {
@@ -164,30 +150,32 @@ pub fn run(allocator: std.mem.Allocator, stdout: *std.Io.Writer, iterations: usi
     defer book.deinit(allocator);
 
     // Set vars
-    // var initial: [17]types.Value = .{ 5, 5, 5, 5, 5, 0, 0, 0, 0, 5, 30, 15, 10, 30, 10, 40, 20 };
-    var initial: [17]types.Value = .{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    var initial: [variable.tunables.len]types.Value = undefined;
+    variable.getValues(&initial);
+
     var initial_delta_plus = initial;
     var initial_delta_minus = initial;
-    var delta: [17]f32 = @splat(0);
+    var delta: [variable.tunables.len]f32 = @splat(0);
 
     // Fit parameters
-    const batch: usize = 1e4;
+    const batch: usize = 1e6;
+    const print_step: usize = 10;
     var plateau_counter: usize = 0;
 
     // Hyper parameters
-    const alpha: f32 = 0.602;
-    const gamma: f32 = 0.101;
+    const alpha: f32 = 0.602; // Learning rate decay
+    const gamma: f32 = 0.101; // Perturbation decay
 
-    const a: f32 = 2;
-    const c: f32 = 4;
-    const A: f32 = 10;
+    const a: f32 = 1.5; // Learning rate scale, chosen such as (a/(A+1)^alpha * 30) is the smallest variation in first iterations after A
+    const c: f32 = 5; // Perturbation scale 1-10% range of parameters
+    const A: f32 = 0.1 * @as(f32, @floatFromInt(iterations)); // Stability delay, should remain < 10% of iterations
 
-    updateVariable(initial[0..17]);
+    updateVariable(&initial);
     const initial_eval: f32 = try eval(book.items[0..batch]);
+    try stdout.print("Initial error: {d}\n", .{initial_eval});
 
     var prng = std.Random.DefaultPrng.init(seed: {
         var seed: u64 = undefined;
-        // get random seed from OS
         try std.posix.getrandom(std.mem.asBytes(&seed));
         break :seed seed;
     });
@@ -197,45 +185,43 @@ pub fn run(allocator: std.mem.Allocator, stdout: *std.Io.Writer, iterations: usi
         const k: f32 = @floatFromInt(k_);
         const ak = a / std.math.pow(f32, k + 1 + A, alpha);
         const ck = c / std.math.pow(f32, k + 1, gamma);
-        // std.debug.print("HERE ck {}\n", .{ck});
-        // std.debug.print("{}\n", .{@max(@min(ck, std.math.maxInt(types.Value)), std.math.minInt(types.Value))});
-        // std.debug.print("magnitude {}\n", .{magnitude});
+
         try computeDelta(rand, &delta, 2 * ck);
-        // std.debug.print("delta {any}", .{delta});
 
         initial_delta_plus = initial;
         initial_delta_minus = initial;
 
         applyDelta(&initial_delta_plus, delta[0..]);
-        updateVariable(initial_delta_plus[0..17]);
+        updateVariable(&initial_delta_plus);
         const v1: f32 = try eval(book.items[0..batch]);
 
         applyDeltaNegative(&initial_delta_minus, delta[0..]);
-        updateVariable(initial_delta_minus[0..17]);
+        updateVariable(&initial_delta_minus);
         const v2: f32 = try eval(book.items[0..batch]);
 
         const match: f32 = v1 - v2;
 
         for (&initial, 0..) |*param, i| {
             if (delta[i] == 0)
-                continue;
-            // std.debug.print("ak * match {}\n", .{ak * match});
-            // std.debug.print("@as(f32, @floatFromInt(delta[i])) {}\n", .{@as(f32, @floatFromInt(delta[i]))});
-            // std.debug.print("ak * match / @as(f32, @floatFromInt(delta[i])) {}\n", .{ak * match / @as(f32, @floatFromInt(delta[i]))});
+                break;
             const variation: types.Value = @intFromFloat(@max(@min(ak * (match / delta[i]), std.math.maxInt(types.Value)), std.math.minInt(types.Value)));
             if (variation == 0)
-                continue;
+                // continue;
+                break;
 
             plateau_counter = 0;
 
             param.* -|= variation;
-            std.debug.print("variation {}\n", .{variation});
+            if (k_ % print_step == 0 and i == 0)
+                std.debug.print("variation {}\n", .{variation});
         }
 
-        plateau_counter += 1;
+        if (k > A)
+            plateau_counter += 1;
 
         // std.debug.print("mean {d:.3}\n", .{(v1 + v2) / 2.0});
-        std.debug.print("mean {d}\n", .{(v1 + v2) / 2.0});
+        if (k_ % print_step == 0)
+            std.debug.print("mean {d}\n", .{(v1 + v2) / 2.0});
 
         // Break if 50 iterations without improvements
         if (plateau_counter >= 50) {
@@ -249,7 +235,7 @@ pub fn run(allocator: std.mem.Allocator, stdout: *std.Io.Writer, iterations: usi
 
     try stdout.print("Found variables:\n", .{});
     for (initial) |param| {
-        try stdout.print("{}, ", .{param});
+        try stdout.print("{:>4}, ", .{param});
     }
     try stdout.print("\n", .{});
 }
--- src/variable.zig
+++ src/variable.zig
@@ -1,24 +1,78 @@
+const std = @import("std");
 const types = @import("types.zig");
+
 const Value = types.Value;
+const ValueTunable = i32;
+// const factor_tunable: ValueTunable = 1000;
+const factor_tunable: ValueTunable = 1;
 
-pub var knight_mobility: Value = 5;
-pub var bishop_mobility: Value = 5;
-pub var rook_mobility: Value = 5;
-pub var queen_mobility: Value = 5;
-pub var king_mobility: Value = 5;
+pub const Tunable = struct {
+    name: []const u8,
+    default: ValueTunable,
+    min: ?ValueTunable = null,
+    max: ?ValueTunable = null,
 
-pub var pawn_threat_knight: Value = 0;
-pub var pawn_threat_bishop: Value = 0;
-pub var pawn_threat_rook: Value = 0;
-pub var pawn_threat_queen: Value = 0;
+    pub inline fn value(self: Tunable) types.Value {
+        return @truncate(@divTrunc(self.default, factor_tunable));
+    }
+};
 
-pub var pawn_defend_king: Value = 5;
-pub var pawn_isolated: Value = 30;
-pub var pawn_doubled: Value = 15;
-pub var pawn_blocked: Value = 10;
-pub var pawn_protection: Value = 30;
+pub const knight_mobility: ValueTunable = 5 * factor_tunable;
+pub const bishop_mobility: ValueTunable = 5 * factor_tunable;
+pub const rook_mobility: ValueTunable = 5 * factor_tunable;
+pub const queen_mobility: ValueTunable = 5 * factor_tunable;
+pub const king_mobility: ValueTunable = 5 * factor_tunable;
 
-pub var bishop_pair: Value = 10;
+pub const pawn_threat_knight: ValueTunable = 0 * factor_tunable;
+pub const pawn_threat_bishop: ValueTunable = 0 * factor_tunable;
+pub const pawn_threat_rook: ValueTunable = 0 * factor_tunable;
+pub const pawn_threat_queen: ValueTunable = 0 * factor_tunable;
 
-pub var rook_open_files: Value = 40;
-pub var rook_semi_open_files: Value = 20;
+pub const pawn_defend_king: ValueTunable = 5 * factor_tunable;
+pub const pawn_isolated: ValueTunable = 30 * factor_tunable;
+pub const pawn_doubled: ValueTunable = 15 * factor_tunable;
+pub const pawn_blocked: ValueTunable = 10 * factor_tunable;
+pub const pawn_protection: ValueTunable = 30 * factor_tunable;
+
+pub const bishop_pair: ValueTunable = 10 * factor_tunable;
+
+pub const rook_open_files: ValueTunable = 40 * factor_tunable;
+pub const rook_semi_open_files: ValueTunable = 20 * factor_tunable;
+
+//    4,    2,    0,    2,  -10,    9,    8,    9,  187,   23,   25,   21,
+//   +5,   +3,   -1,   +3,  -11,   +8,   +9,  +12,  +34,  +20,  +28,  +18,
+pub var tunables = [_]Tunable{
+    .{ .name = "knight_mobility", .default = knight_mobility },
+    .{ .name = "bishop_mobility", .default = bishop_mobility },
+    .{ .name = "rook_mobility", .default = rook_mobility },
+    .{ .name = "queen_mobility", .default = queen_mobility },
+    // .{ .name = "king_mobility", .default = king_mobility },
+    // .{ .name = "pawn_threat_knight", .default = pawn_threat_knight },
+    // .{ .name = "pawn_threat_bishop", .default = pawn_threat_bishop },
+    // .{ .name = "pawn_threat_rook", .default = pawn_threat_rook },
+    // .{ .name = "pawn_threat_queen", .default = pawn_threat_queen },
+    .{ .name = "pawn_defend_king", .default = pawn_defend_king },
+    .{ .name = "pawn_isolated", .default = pawn_isolated },
+    .{ .name = "pawn_doubled", .default = pawn_doubled },
+    .{ .name = "pawn_blocked", .default = pawn_blocked },
+    .{ .name = "pawn_protection", .default = pawn_protection },
+    .{ .name = "bishop_pair", .default = bishop_pair },
+    .{ .name = "rook_open_files", .default = rook_open_files },
+    .{ .name = "rook_semi_open_files", .default = rook_semi_open_files },
+};
+
+pub fn getValues(buffer: []types.Value) void {
+    std.debug.assert(buffer.len >= tunables.len);
+    for (tunables, 0..) |tunable, i| {
+        buffer[i] = tunable.value();
+    }
+}
+
+pub fn getValue(name: []const u8) types.Value {
+    inline for (tunables) |tunable| {
+        if (std.ascii.eqlIgnoreCase(tunable.name, name)) {
+            return tunable.value();
+        }
+    }
+    unreachable;
+}
