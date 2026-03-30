29274ded87d09ff734e70c2eae21c5a29030aec5
Paul-Elie Pipelin <paul.elie97@gmail.com>
2026-03-30 20:29:00 +0200

    Add game phase bench: 15091544

--- src/evaluate.zig
+++ src/evaluate.zig
@@ -288,9 +288,9 @@ pub fn evaluateTable(pos: position.Position) types.Value {
         }
     }
 
-    const tapered: i64 = @divTrunc(@as(i64, pos.score_material_w + pos.score_material_b - 2 * tables.material[types.PieceType.king.index()]) * 10_000, (4152 * 2));
-    score +|= @truncate(@divTrunc(tapered * pos.score_mg, 10_000));
-    score +|= @truncate(@divTrunc((10_000 - tapered) * pos.score_eg, 10_000));
+    const tapered: i32 = pos.state.phase[types.Color.white.index()] + pos.state.phase[types.Color.black.index()];
+    score +|= @truncate(@divTrunc(tapered * pos.score_mg, tables.phase_total_2));
+    score +|= @truncate(@divTrunc((tables.phase_total_2 - tapered) * pos.score_eg, tables.phase_total_2));
 
     return if (pos.state.turn.isWhite()) score else -score;
 }
