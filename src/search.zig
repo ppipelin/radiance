0f31a8b47d173091ef358151b497c5bdcd28774e
Paul-Elie Pipelin <paul.elie97@gmail.com>
2026-04-15 19:50:00 +0200

    Remove tt filling in move loop bench: 6824109

--- src/search.zig
+++ src/search.zig
@@ -528,14 +528,6 @@ fn abSearch(allocator: std.mem.Allocator, comptime nodetype: NodeType, noalias s
                 // Only for first move (PVS) or after a fail high
                 if (pv_node and (move_count == 1 or score > alpha)) {
                     score = -try abSearch(allocator, NodeType.pv, ss + 1, pos, limits, eval, -beta, -alpha, depth - 1 + @intFromBool(pos.state.checkers != 0), is_960, false);
-                    // Let's assert we don't store draw (repetition)
-                    if (score != types.value_draw) {
-                        if (found == null or tt_depth <= depth) {
-                            const tt_flag: types.TableBound = if (score >= beta) .lowerbound else if (alpha != alpha_) .exact else .upperbound;
-
-                            try tables.transposition_table.put(allocator, key, .{ types.valueToTT(score, ss[0].ply), depth, move, tt_flag });
-                        }
-                    }
                 }
             }
         }
