b55e36ffc30ce3a2b61f86c49e69afbc8df82c04
Paul-Elie Pipelin <paul.elie97@gmail.com>
2026-09-11 14:57:54 +0200

    Change to smaller L1 bench: 2312516

--- src/Nnue.zig
+++ src/Nnue.zig
@@ -15,7 +15,7 @@ pub const lanes: comptime_int = std.simd.suggestVectorLength(Quantized) orelse 1
 pub const lanes_full: comptime_int = std.simd.suggestVectorLength(Full) orelse 1;
 
 pub const input_size: usize = 768; // L0
-pub const hidden_size: usize = 512; // L1
+pub const hidden_size: usize = 128; // L1
 pub const output_size: usize = 8; // Output buckets number
 const divisor: usize = std.math.divCeil(usize, 32, output_size) catch unreachable;
 const quantization_a = 255;
